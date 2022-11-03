7c74b55b9ef9c4f8434e4ddc93c87d19457d2abc
xxx <xxx.com>
2022-11-03 13:33:56 +0800

    A! from Ubuntu desktop

--- blog/processBlog.sh
+++ blog/processBlog.sh
@@ -1,7 +1,8 @@
-python3 /media/lh/Data/Rice_Bowl/BLOG/blog/main.py
-cd /media/lh/Data/Rice_Bowl/BLOG
+python3 $BLOG_ROOT/blog/main.py
+cd $BLOG_ROOT
+git pull
 git add .
 echo  'enter the message'
-git commit -m 'A!' 
+git commit -m "A! from $IDENTIFIER_LH"
 git push -u origin gh-pages-mytest
 
