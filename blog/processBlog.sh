c06c2bf7b032a6fdae3c3d4aaee9b1c58767e83d
xxx <xxx.com>
2022-11-05 22:16:37 +0800

    A! from Ubuntu desktop

--- blog/processBlog.sh
+++ blog/processBlog.sh
@@ -1,8 +1,13 @@
 python3 $BLOG_ROOT/blog/main.py
-cd $BLOG_ROOT
-git pull
-git add .
-echo  'enter the message'
-git commit -m "A! from $IDENTIFIER_LH"
-git push -u origin gh-pages-mytest
+read -n1 -s -r -p $'Press space to continue...\n' key
+
+read -rsp $'Press any key to continue...\n' -n1 key
+    cd $BLOG_ROOT
+    git pull
+    git add .
+    echo  'enter the message'
+    git commit -m "A! from $IDENTIFIER_LH"
+    git push -u origin gh-pages-mytest
+
+
 
