2cf5acc8ad5b5df4b89c0eb512b86e3753fcac5a
bxrjmfh <qq376350262@163.com>
2023-03-25 00:00:49 +0800

    A! from Ubuntu desktop

--- blog/processBlog.sh
+++ blog/processBlog.sh
@@ -1,12 +1,13 @@
+cd $BLOG_ROOT
+git pull
 python3 $BLOG_ROOT/blog/main.py $RAW_BLOG_PATH $POST_FILES_PATH
 
 read -rsp $'Press any key to continue...\n' -n1 key
-    cd $BLOG_ROOT
-    git pull
-    git add .
-    echo  'enter the message'
-    git commit -m "A! from $IDENTIFIER_LH"
-    git push -u origin gh-pages-mytest
+
+git add .
+echo  'enter the message'
+git commit -m "A! from $IDENTIFIER_LH"
+git push -u origin gh-pages-mytest
 
 
 
